6028a2847e8793a8603d019481a3301483d23165
Angel Carballo <contact@angelcarballo.com>
2017-02-26 00:54:40 +0000

    Use melpa as primary package source

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -4,8 +4,8 @@
 ;; Packages
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/"))
 (add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
+(add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/"))
 (add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
 
 (setq package-enable-at-startup nil)
