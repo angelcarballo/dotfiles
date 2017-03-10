db91893b330b660efd6aedbc76ce2d1f48196468
Angel Carballo <contact@angelcarballo.com>
2017-03-10 07:29:31 +0000

    Restore zsh color config (needed for prompt)

--- zsh/.zsh/theme.zsh
+++ zsh/.zsh/theme.zsh
@@ -27,3 +27,28 @@ PROMPT='$(current_path) %B%F{yellow}${vcs_info_msg_0_}%B%F{yellow} %{$reset_colo
 
 autoload -U add-zsh-hook
 add-zsh-hook precmd  theme_precmd
+
+########### Dircolors ###########
+
+autoload colors; colors;
+
+LS_COLORS="di=01;34:ln=01;36:pi=40;33:so=01;35:do=01;35:bd=40;33;01:cd=40;33;01:or=01;05;37;41:mi=01;05;37;41:su=37;41:sg=30;43:tw=30;42:ow=34;42:st=37;44:ex=01;32";
+LSCOLORS="ExGxFxDxCxDxDxhbhdacEc";
+
+# Do we need Linux or BSD Style?
+if ls --color -d . &>/dev/null 2>&1
+then
+  # Linux Style
+  export LS_COLORS=$LS_COLORS
+  alias ls='ls --color=tty'
+else
+  # BSD Style
+  export LSCOLORS=$LSCOLORS
+  alias ls='ls -G'
+fi
+
+# Use same colors for autocompletion
+zmodload -a colors
+zmodload -a autocomplete
+zmodload -a complist
+zstyle ':completion:*:default' list-colors ${(s.:.)LS_COLORS}
