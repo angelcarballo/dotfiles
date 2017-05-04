784dd55660a137c704f5cd2b91ae3fd20abb89ab
Angel Carballo <contact@angelcarballo.com>
2017-05-04 11:56:29 +0100

    Add $make_public method to ruby repls

--- ruby/.ruby_repl_ext.rb
+++ ruby/.ruby_repl_ext.rb
@@ -23,3 +23,10 @@ def clear
   system 'clear'
 end
 
+# Make all private methods public for the given object
+def make_public(obj)
+  obj.private_methods.each do |method_name|
+    obj.singleton_class.class_eval { public method_name }
+  end
+  obj
+end
