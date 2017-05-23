2dec9b7cb9e675b7290e710dc818c6c42fa80386
Angel Carballo <contact@angelcarballo.com>
2017-05-23 09:14:03 +0100

    Add copy commands to ruby repls

--- ruby/.ruby_repl_ext.rb
+++ ruby/.ruby_repl_ext.rb
@@ -7,9 +7,9 @@
   end
 end
 
-# This extension adds a UNIX-style pipe to strings
-# => puts 'foobar' | 'wc'
 class String
+  # This extension adds a UNIX-style pipe to strings
+  # => puts 'foobar' | 'wc'
   def |(cmd)
     IO.popen(cmd.to_s, 'r+') do |pipe|
       pipe.write(self)
@@ -18,15 +18,23 @@ class String
     end
   end
 
+  # Copy to system clipboard using pbcopy (only MacOs)
   def copy
     self.public_send('|', 'pbcopy')
     self
   end
 end
 
+# Copy to system clipboard using pbcopy (only MacOs)
+def copy(obj)
+  obj.to_s.copy
+end
+
+# Cleanup current shell
 def clear
   system 'clear'
 end
+
 
 # Make all private methods public for the given object
 def make_public(obj)
