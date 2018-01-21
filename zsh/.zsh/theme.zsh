7927bfc43b5202ce29b13cd67abbf575fe85e353
Angel Carballo <contact@angelcarballo.com>
2018-01-21 09:50:46 +0000

    Cleanup zsh theme
    Syntax was incorrect, don't use bold and avoid leaking into shell

--- zsh/.zsh/theme.zsh
+++ zsh/.zsh/theme.zsh
@@ -1,4 +1,3 @@
-
 ########### Prompt (based on: https://github.com/sindresorhus/pure) ###########
 
 autoload -Uz vcs_info
@@ -8,22 +7,26 @@ zstyle ':vcs_info:*' stagedstr '∙'
 zstyle ':vcs_info:*' unstagedstr '∙'
 zstyle ':vcs_info:(sv[nk]|bzr):*' branchformat '%b%F{1}:%F{11}%r'
 zstyle ':vcs_info:*' enable git
-zstyle ':vcs_info:*' formats '@%b%c%u%B'
+zstyle ':vcs_info:*' formats '@%b%c%u'
 
 theme_precmd() {
   vcs_info
 }
 
 current_path() {
-  echo '%B%F{blue}%~'
+  echo '%F{blue}%~%F{reset_color}'
+}
+
+current_branch() {
+  echo "%F{yellow}${vcs_info_msg_0_}%F{reset_color}"
 }
 
 prompt_symbol_with_last_command_status() {
-  echo "%(?.%{$reset_color%}.%{$fg[red]%})>%{$reset_color%}"
+  echo "%(?.%F{white}.%F{red})>%F{reset_color}"
 }
 
 setopt prompt_subst
-PROMPT='$(current_path) %B%F{yellow}${vcs_info_msg_0_}%B%F{yellow} %{$reset_color%}% $(prompt_symbol_with_last_command_status) '
+PROMPT='$(current_path) $(current_branch) $(prompt_symbol_with_last_command_status) '
 
 autoload -U add-zsh-hook
 add-zsh-hook precmd  theme_precmd
