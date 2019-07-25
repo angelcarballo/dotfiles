681dc949111a40a8c1d77330117dc1952552bba4
Angel Carballo <contact@angelcarballo.com>
2019-07-25 10:58:32 +0100

    Add ssid function to print connected network SSID

--- zsh/.zsh/custom-functions.zsh
+++ zsh/.zsh/custom-functions.zsh
@@ -27,3 +27,9 @@ ss() {
     ts $(echo "$project")
 }
 # }}}
+
+# Current WIFI network name {{{
+ssid() {
+  /System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport -I | awk '/ SSID/ {print substr($0, index($0, $2))}'
+}
+# }}}
