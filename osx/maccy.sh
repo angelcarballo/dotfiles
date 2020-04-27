c5e0da6bd044aa1a81ce8be0becda984e441c270
Angel Carballo <contact@angelcarballo.com>
2020-04-27 08:55:26 +0100

    Enable fuzzy search on maccy

--- osx/maccy.sh
+++ osx/maccy.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
 # Maccy clipboard manager
+
+# trigger mapping
 defaults write org.p0deje.Maccy hotKey command+option+v
+
+# paste automatically on selection
 defaults write org.p0deje.Maccy pasteByDefault true
+
+# enable fuzzy search
+defaults write org.p0deje.Maccy fuzzySearch true
