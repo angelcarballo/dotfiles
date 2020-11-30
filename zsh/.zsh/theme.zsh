71cd582b630fb6f7c379b4482cf482d3bbcbd2bd
Angel Carballo <contact@angelcarballo.com>
2020-11-30 20:39:45 +0000

    Add zsh vim status indicator to prompt

--- zsh/.zsh/theme.zsh
+++ zsh/.zsh/theme.zsh
@@ -1,3 +1,5 @@
+# Gitstatus {{{
+
 # Load gitstatus plugin
 source ${GITSTATUS_DIR}/gitstatus.plugin.zsh || return
 
@@ -44,12 +46,41 @@ gitstatus_stop MY && gitstatus_start MY
 autoload -Uz add-zsh-hook
 add-zsh-hook precmd gitstatus_prompt_update
 
+# }}}
+
+
+# Vim mode indicator {{{
+vim_ins_mode="$ "
+vim_cmd_mode="$•"
+vim_mode=$vim_ins_mode
+
+function zle-keymap-select {
+  vim_mode="${${KEYMAP/vicmd/${vim_cmd_mode}}/(main|viins)/${vim_ins_mode}}"
+  zle reset-prompt
+}
+zle -N zle-keymap-select
+
+function zle-line-finish {
+  vim_mode=$vim_ins_mode
+}
+zle -N zle-line-finish
+
+# Fix a bug when you C-c in CMD mode and you'd be prompted with CMD mode indicator, while in fact you would be in INS mode
+# Fixed by catching SIGINT (C-c), set vim_mode to INS and then repropagate the SIGINT, so if anything else depends on it, we will not break it
+# Thanks Ron! (see comments)
+function TRAPINT() {
+  vim_mode=$vim_ins_mode
+  return $(( 128 + $1 ))
+}
+# }}}
+
+
 current_path() {
   echo '%F{12}%~%F{reset_color}'
 }
 
 prompt_symbol_with_last_command_status() {
-  echo "%-30(l::\n)%(?.%F{white}.%F{red})$%F{reset_color}"
+  echo "%-30(l::\n)%(?.%F{white}.%F{red})${vim_mode}%F{reset_color}"
 }
 
 username() {
