031c4d8a81124e86447bd03f8b6e97c77f7a1e80
Angel Carballo <contact@angelcarballo.com>
2020-12-06 17:38:29 +0000

    Remove unused make_public ruby function

--- ruby/.ruby_repl_ext.rb
+++ ruby/.ruby_repl_ext.rb
@@ -34,12 +34,3 @@ end
 def clear
   system 'clear'
 end
-
-
-# Make all private methods public for the given object
-def make_public(obj)
-  obj.private_methods.each do |method_name|
-    obj.singleton_class.class_eval { public method_name }
-  end
-  obj
-end
