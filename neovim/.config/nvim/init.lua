6063a0a19bd54d4feb2a0cf5a64f426a5b88e5a2
Angel Carballo <angel@duffel.com>
2022-05-13 08:33:12 +0100

    Nvim: do not auto select autocompletion options

--- neovim/.config/nvim/init.lua
+++ neovim/.config/nvim/init.lua
@@ -427,7 +427,7 @@ cmp.setup({
     ['<C-k>'] = cmp.mapping.scroll_docs(-4),
     ['<C-j>'] = cmp.mapping.scroll_docs(4),
     ['<C-e>'] = cmp.mapping.abort(),
-    ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+    ['<CR>'] = cmp.mapping.confirm({ select = false }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
   },
   sources = cmp.config.sources({
     { name = 'nvim_lsp' },
