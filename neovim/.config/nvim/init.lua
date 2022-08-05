2e2137f13ce625a285566f5bc921d8e86ec70abb
Angel Carballo <angel@duffel.com>
2022-08-05 21:12:32 +0100

    Nvim: fix catppuccin config

--- neovim/.config/nvim/init.lua
+++ neovim/.config/nvim/init.lua
@@ -448,26 +448,8 @@ vim.g.solarized_statusline = 'flat' -- flat variant has a less distracting statu
 vim.g.solarized_extra_hi_groups = 1 -- show filetype specific highlight groups
 
 -- Colorscheme: catppuccin
+vim.g.catppuccin_flavour = "mocha" -- latte, frappe, macchiato, mocha
 require("catppuccin").setup({
-  integrations = {
-    native_lsp = {
-      enabled = true,
-      virtual_text = {
-        errors = "italic",
-        hints = "italic",
-        warnings = "italic",
-        information = "italic",
-      },
-      underlines = {
-        errors = "underline",
-        hints = "underline",
-        warnings = "underline",
-        information = "underline",
-      },
-    },
-    gitsigns = false,
-    telescope = true,
-  },
 })
 --   }}}
 
