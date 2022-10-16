f394f5f5e77c48d339b79b78faacb36b1722004e
Angel Carballo <angel@duffel.com>
2022-10-16 19:13:57 +0100

    Nvim: cleanup snippets file

--- neovim/.config/nvim/lua/snippets.lua
+++ neovim/.config/nvim/lua/snippets.lua
@@ -1,39 +1,51 @@
+-- Local shortcuts as suggested in https://github.com/L3MON4D3/Luas/blob/master/DOC.md#loaders
 local ls = require("luasnip")
-
--- some shorthands...
-local snip = ls.snippet
--- local node = ls.snippet_node
-local text = ls.text_node
-local insert = ls.insert_node
--- local func = ls.function_node
--- local choice = ls.choice_node
--- local dynamicn = ls.dynamic_node
+local s = ls.snippet
+-- local sn = ls.snippet_node
+-- local isn = ls.indent_snippet_node
+local t = ls.text_node
+local i = ls.insert_node
+-- local f = ls.function_node
+-- local c = ls.choice_node
+-- local d = ls.dynamic_node
+-- local r = ls.restore_node
+-- local events = require("luasnip.util.events")
+-- local ai = require("luasnip.nodes.absolute_indexer")
+-- local fmt = require("luasnip.extras.fmt").fmt
+-- local extras = require("luasnip.extras")
+-- local m = extras.m
+-- local l = extras.l
+-- local rep = extras.rep
+-- local postfix = require("luasnip.extras.postfix").postfix
 
 ls.add_snippets(nil, {
   elixir = {
-    snip({trig = "fn", namr = "fn", dscr = "Lambda function"}, {
-      text("fn "),
-      insert(1, "args"),
-      text(" -> "),
-      insert(2, "body"),
-      text(" end ")
+    s({trig = "fn", name = "fn", dscr = "Lambda function"}, {
+      t("fn "),
+      i(1, "args"),
+      t(" -> "),
+      i(2, "body"),
+      t(" end ")
     }),
-    snip({trig = "echo", namr = "echo", dscr = "Inspect with label"}, {
-      text("IO.inspect("),
-      insert(1, "variable"),
-      text(", label: \""),
-      insert(2, "label"),
-      text("\")")
+
+    s({trig = "echo", name = "echo", dscr = "Inspect with label"}, {
+      t("IO.inspect("),
+      i(1, "variable"),
+      t(", label: \""),
+      i(2, "label"),
+      t("\")")
     }),
-    snip({trig = "pecho", namr = "pecho", dscr = "Inspect in pipeline"}, {
-      text("|> IO.inspect(label: \""),
-      insert(1, "label"),
-      text("\")")
+
+    s({trig = "pecho", name = "pecho", dscr = "Inspect in pipeline"}, {
+      t("|> IO.inspect(label: \""),
+      i(1, "label"),
+      t("\")")
     }),
-    snip({trig = "shout", namr = "shout", dscr = "Shout to terminal"}, {
-      text("IO.puts(\"========> "),
-      insert(1, "text"),
-      text("\")")
+
+    s({trig = "shout", name = "shout", dscr = "Shout to terminal"}, {
+      t("IO.puts(\"========> "),
+      i(1, "t"),
+      t("\")")
     })
   }
 })
