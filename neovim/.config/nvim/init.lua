3c7d699521bc3fce1a892e4ca41a682749658f9c
Angel Carballo <angel@duffel.com>
2022-10-19 20:48:04 +0100

    Nvim: display borders on lsp popups

--- neovim/.config/nvim/init.lua
+++ neovim/.config/nvim/init.lua
@@ -740,6 +740,14 @@ end
 
 local capabilities = require('cmp_nvim_lsp').default_capabilities()
 
+-- Display borders
+local orig_util_open_floating_preview = vim.lsp.util.open_floating_preview
+function vim.lsp.util.open_floating_preview(contents, syntax, opts, ...)
+  opts = opts or {}
+  opts.border = opts.border or "single"
+  return orig_util_open_floating_preview(contents, syntax, opts, ...)
+end
+
 -- efm-langserver runs credo
 require 'lspconfig'.efm.setup{
   filetypes = {'elixir', 'eelixir'};
