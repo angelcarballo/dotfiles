346033203fa0068508eda8ed827fd69ee2c88eb1
Angel Carballo <angel@duffel.com>
2022-12-25 10:19:06 +0000

    Nvim: don't auto reload init.lua

--- neovim/.config/nvim/init.lua
+++ neovim/.config/nvim/init.lua
@@ -106,14 +106,6 @@ if is_bootstrap then
   print '=================================='
   return
 end
-
--- Automatically source and re-compile packer whenever you save this init.lua
-local packer_group = vim.api.nvim_create_augroup('Packer', { clear = true })
-vim.api.nvim_create_autocmd('BufWritePost', {
-  command = 'source <afile>',
-  group = packer_group,
-  pattern = vim.fn.expand '$MYVIMRC',
-})
 -- }}}
 -- Plugin Settings {{{
 --   vim-dirvish {{{
