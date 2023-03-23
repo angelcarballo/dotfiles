74f9f964c2fe3671fa6ab08e39255f2e9330d1c9
Angel Carballo <angel@duffel.com>
2023-03-23 13:51:15 +0000

    Nvim: complete with tab only
    Previous behaviour confirming with CR causes unwanted completions and
    it's annoying in general.

--- neovim/.config/nvim/init.lua
+++ neovim/.config/nvim/init.lua
@@ -813,28 +813,24 @@ cmp.setup {
   mapping = cmp.mapping.preset.insert {
         ['<C-d>'] = cmp.mapping.scroll_docs(-4),
         ['<C-f>'] = cmp.mapping.scroll_docs(4),
-        ['<CR>'] = cmp.mapping.confirm {
+        ['<Tab>'] = cmp.mapping.confirm {
       behavior = cmp.ConfirmBehavior.Replace,
       select = true,
     },
-        ['<Tab>'] = cmp.mapping(function(fallback)
+        ['<C-n>'] = cmp.mapping(function(fallback)
       if cmp.visible() then
         cmp.select_next_item()
-      elseif luasnip.expand_or_jumpable() then
-        luasnip.expand_or_jump()
       else
         fallback()
       end
     end, { 'i', 's' }),
-        ['<S-Tab>'] = cmp.mapping(function(fallback)
+        ['<C-p>'] = cmp.mapping(function(fallback)
       if cmp.visible() then
         cmp.select_prev_item()
-      elseif luasnip.jumpable(-1) then
-        luasnip.jump(-1)
       else
         fallback()
       end
-    end, { 'i', 's' }),
+    end, { 'i', 's' })
   },
   sources = {
     { name = 'nvim_lsp' },
