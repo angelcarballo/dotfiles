e36e11ea75cb7c92a367b22e34b0842e43a76ea9
Angel Carballo <angel@duffel.com>
2023-05-31 08:30:01 +0100

    Nvim: syntax for vim help no longer available

--- neovim/.config/nvim/init.lua
+++ neovim/.config/nvim/init.lua
@@ -225,7 +225,7 @@ require("auto-session").setup({
 -- {{{ treesitter
 require('nvim-treesitter.configs').setup {
   -- Add languages to be installed here that you want installed for treesitter
-  ensure_installed = { 'elixir', 'python', 'ruby', 'javascript', 'typescript', 'help', 'lua' },
+  ensure_installed = { 'elixir', 'python', 'ruby', 'javascript', 'typescript', 'lua' },
 
   highlight = { enable = true },
   indent = { enable = true },
