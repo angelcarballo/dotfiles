b98850235d7ec7d346f5eae7490e086ab633a8bb
Angel Carballo <angel@duffel.com>
2024-01-11 20:09:16 +0000

    Nvim: don't use cmp for command line

--- neovim/.config/nvim/init.lua
+++ neovim/.config/nvim/init.lua
@@ -247,14 +247,6 @@ require('lazy').setup({
       --       { name = 'buffer' },
       --     })
       -- })
-
-      -- Use buffer source for `/` and `?` (if you enabled `native_menu`, this won't work anymore).
-      cmp.setup.cmdline({ '/', '?' }, {
-        mapping = cmp.mapping.preset.cmdline(),
-        sources = {
-          { name = 'buffer' }
-        }
-      })
     end,
   },
 
