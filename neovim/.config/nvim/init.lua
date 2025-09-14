c82f3ab4f0dfc891c97702b3ddc5514c39206189
Angel Carballo <angel@duffel.com>
2025-09-14 15:41:53 +0100

    Nvim: remove parrot

--- neovim/.config/nvim/init.lua
+++ neovim/.config/nvim/init.lua
@@ -439,22 +439,6 @@ require('lazy').setup({
   'bullets-vim/bullets.vim',
 
   {
-    "frankroeder/parrot.nvim",
-    dependencies = { 'ibhagwan/fzf-lua', 'nvim-lua/plenary.nvim' },
-    -- optionally include "folke/noice.nvim" or "rcarriga/nvim-notify" for beautiful notifications
-    config = function()
-      require("parrot").setup {
-        -- Providers must be explicitly added to make them available.
-        providers = {
-          anthropic = {
-            api_key = os.getenv "ANTHROPIC_API_KEY",
-          }
-        },
-      }
-    end,
-  },
-
-  {
     "nvim-treesitter/nvim-treesitter-context",
     config = function()
       require'treesitter-context'.setup{
